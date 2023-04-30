32b23cbee8c31d8b6a90b957f272751bfd14e471
HeyBlack233 <88659388+HeyBlack233@users.noreply.github.com>
2023-04-30 12:47:23 +0800

    Add files via upload

--- src/main/java/heyblack/betterarmorswap/mixin/ClientPlayerInteractionManagerMixin.java
+++ src/main/java/heyblack/betterarmorswap/mixin/ClientPlayerInteractionManagerMixin.java
@@ -5,38 +5,38 @@ import net.fabricmc.api.Environment;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Equipment;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Wearable;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.screen.slot.SlotActionType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Environment(EnvType.CLIENT)
 @Mixin(ClientPlayerInteractionManager.class)
 public abstract class ClientPlayerInteractionManagerMixin
 {
-    @Shadow public abstract ItemStack clickSlot(int syncId, int slotId, int clickData, SlotActionType actionType, PlayerEntity player);
+    @Shadow public abstract void clickSlot(int syncId, int slotId, int button, SlotActionType actionType, PlayerEntity player);
 
     @Inject(method = "clickSlot", at = @At("HEAD"), cancellable = true)
-    public void swap(int syncId, int slotId, int clickData, SlotActionType actionType, PlayerEntity player, CallbackInfoReturnable<ItemStack> cir)
+    public void swap(int syncId, int slotId, int button, SlotActionType actionType, PlayerEntity player, CallbackInfo ci)
     {
         if (slotId >= 0)
         {
             Slot slot = player.currentScreenHandler.getSlot(slotId);
             ItemStack itemStack = slot.getStack();
-            if (actionType == SlotActionType.QUICK_MOVE && itemStack.getItem() instanceof Wearable)
+            if (actionType == SlotActionType.QUICK_MOVE && itemStack.getItem() instanceof Equipment)
             {
                 int equipmentSlotId = 8 - MobEntity.getPreferredEquipmentSlot(itemStack).getEntitySlotId();
-                if (slotId != equipmentSlotId && clickData == 1 && player.currentScreenHandler.getSlot(equipmentSlotId).getStack().getItem() instanceof Wearable)
+                if (slotId != equipmentSlotId && button == 1 && player.currentScreenHandler.getSlot(equipmentSlotId).getStack().getItem() instanceof Equipment)
                 {
                     clickSlot(syncId, slotId, 0, SlotActionType.PICKUP, player);
                     clickSlot(syncId, equipmentSlotId, 0, SlotActionType.PICKUP, player);
                     clickSlot(syncId, slotId, 0, SlotActionType.PICKUP, player);
-                    cir.cancel();
+                    ci.cancel();
                 }
             }
         }
